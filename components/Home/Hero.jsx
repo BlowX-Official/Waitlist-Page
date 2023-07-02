270604dbb61e33e4e9af1d6272336a712dd6f355
AYENI DANIEL <starlordflash@gmail.com>
2023-07-02 15:41:13 +0100

    pushed

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -29,6 +29,52 @@ const Hero = () => {
   const [openForm, setOpenForm] = useState(false);
   const [openSuccess, setOpenSuccess] = useState(false);
   const [selected, setSelected] = useState({});
+
+  // coundown
+  
+   const [countdown, setCountdown] = useState({
+     days: 0,
+     hours: 0,
+     minutes: 0,
+     seconds: 0,
+   });
+
+   useEffect(() => {
+     const interval = setInterval(() => {
+       const targetDate = new Date("2023-08-02"); // Set your target date here
+       const now = new Date();
+       const timeDifference = targetDate - now;
+
+       if (timeDifference <= 0) {
+         clearInterval(interval);
+       } else {
+         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+         const hours = Math.floor(
+           (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+         );
+         const minutes = Math.floor(
+           (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+         );
+         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+         setCountdown({ days, hours, minutes, seconds });
+       }
+     }, 1000);
+
+     return () => {
+       clearInterval(interval);
+     };
+   }, []);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // coundown
   
   // const handleSubmit = () => {
   //   saveEmail(email);
@@ -92,7 +138,10 @@ const Hero = () => {
             Join us to explore the opportunity.
           </h1>
           <h1 className="text-white font-bold text-center text-xl md:text-[32px] md:leading-[25px] mt-8 md:mt-[50px]">
-            7D : 20H : 3M : 19S
+            <span>{`${countdown.days}D`}</span> :{" "}
+            <span>{`${countdown.hours}H`}</span> :{" "}
+            <span>{`${countdown.minutes}M`}</span> :{" "}
+            <span>{`${countdown.seconds}S`}</span>
           </h1>
           <button
             onClick={() => setOpenForm(!openForm)}
